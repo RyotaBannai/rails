df35465353e0399e711b22623129238dde090911
RyotaBannai <ryotala0528@gmail.com>
2020-07-12 18:41:01 +0900

    read docs getting_started

--- playground/starter_app/app/controllers/users_controller.rb
+++ playground/starter_app/app/controllers/users_controller.rb
@@ -22,8 +22,11 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    # puts params.inspect # show request on terminal
+    # Rails.logger.debug params.inspect # this is the better way.
+    # <%= params.inspect %> in erb
 
+    @user = User.new(user_params)
     respond_to do |format|
       if @user.save
         format.html do
